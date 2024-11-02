f20c631b68423e0f43a0cf6755fcdd2da266b3ef
Carlos Beltran <imthatcarlos@gmail.com>
2024-11-02 15:24:37 -0500

    cron

--- core/src/clients/cron/index.ts
+++ core/src/clients/cron/index.ts
@@ -12,19 +12,23 @@ class AdminClient {
     this.agents = agents;
 
     this.app.post("/cron/:agentId/post", async (req: express.Request, res: express.Response) => {
-      // verify lens jwt token
-      const { text } = req.body;
+      const token = req.headers['authorization'] as string;
+      if (token !== `Bearer ${process.env.CRON_SECRET!}`) {
+        res.status(401).send("Invalid authorization header");
+        return;
+      }
+
       const { agentId } = req.params;
-
-      // TODO: should be callable only by vercel cron
-
       const wallets = await getWallets(agentId, true);
+
       if (!wallets?.polygon) res.status(500).send("failed to load polygon wallet");
 
-      // TODO: choose what to post right now
+      // TODO: choose what to post right now; imageURL should be hosted by us
+      const text = undefined;
+      const imageURL = undefined;
 
       try {
-        const success = await createPost(wallets?.polygon, wallets?.profile.id, text);
+        const success = await createPost(wallets?.polygon, wallets?.profile.id, text, imageURL);
 
         res.status(!!success ? 200 : 400).json();
       } catch (error) {
